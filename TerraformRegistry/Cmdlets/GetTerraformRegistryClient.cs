abe1b56207e91f29b439d6985d4e35fb642b275f
Jeffrey Patton <jeffrey@patton-tech.com>
2024-08-20 17:01:59 -0500

    Added a GetTerraformClient cmdlet

---
+++ TerraformRegistry/Cmdlets/GetTerraformRegistryClient.cs
@@ -0,0 +1,27 @@
+﻿using ModPosh.TerraformRegistry.Common;
+using ModPosh.TerraformRegistryClient;
+using System.Management.Automation;
+
+namespace ModPosh.TerraformRegistry.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "TerraformRegistryClient")]
+    public class GetTerraformRegistryClientCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var client = TerraformRegistryClientManager.TerraformRegistryClient;
+
+            if (client == null)
+            {
+                WriteError(new ErrorRecord(
+                    new InvalidOperationException("The TerraformRegistryClient has not been initialized."),
+                    "ClientNotInitialized",
+                    ErrorCategory.InvalidOperation,
+                    null));
+                return;
+            }
+
+            WriteObject(client);
+        }
+    }
+}
